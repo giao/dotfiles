24f954a066a638bc85fb9ec6bc9a211116257edd
Hubert depesz Lubaczewski <depesz@depesz.com>
2017-05-01 12:21:34 +0200

    add strict mode to all shell scripts

--- .vim/template/template.sh
+++ .vim/template/template.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+
+# Unofficial Bash Strict Mode
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+# End of Unofficial Bash Strict Mode
+
 <+CURSOR+>
